37f120eb4c7d09416e845598555485050e30d820
ARizeDevs <arize@arize.io>
2022-10-31 14:38:10 +0100

    som changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 import '../scss/app.scss';
 
-
-import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
-import * as tf from '@tensorflow/tfjs-core';
-// Register WebGL backend.
-import '@tensorflow/tfjs-backend-webgl';
 import './demo.js';
